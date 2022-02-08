6c7a8748d661150cf1ea0e99af3ec70c70f31365
NataliaStupka <NataliGluschenko@ukr.net>
2022-02-08 20:44:31 +0200

    fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ImageGallery from './ImageGallery/ImageGallery';
 
 class App extends Component {
   state = {};
 
   render() {
     return <div>App</div>;
+    <ImageGallery />;
   }
 }
 
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,7 +1,34 @@
-
 //Список карточек изображений
+import { Component } from 'react';
 
-<ul class="gallery">
-  <li>  Набор li с изображениями </li>
-</ul>
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
 
+export default class ImageGallery extends Component {
+  state = {
+    card: {},
+    error: null,
+    status: Status.IDLE,
+  };
+
+  componentDidMount() {
+    const API_Key = '24451783-36fc53d78d658727e466a2b4b';
+    fetch(
+      `https://pixabay.com/api/?q=cat&page=1&key=${API_Key}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(res => res.json())
+      .then(console.log);
+  }
+
+  render() {
+    return <div>{this.state.card && <div>Card image</div>}</div>;
+  }
+}
+
+// <ul class="gallery">
+//   <li> Набор li с изображениями </li>
+// </ul>;
