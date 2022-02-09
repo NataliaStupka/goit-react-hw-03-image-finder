a748aa9eb75dc8a7f97afb53221969507113ac2f
NataliaStupka <NataliGluschenko@ukr.net>
2022-02-09 12:19:06 +0200

    на ноуте не подтягивает???

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,59 +1,49 @@
 import { Component } from 'react';
-import { BiSearchAlt2 } from "react-icons/bi";
-
+import { BiSearchAlt2 } from 'react-icons/bi';
 
 export default class Searchbar extends Component {
-    state = {
-        imageName: '',
-    };
+  state = {
+    imageName: '',
+  };
 
- //обновляет imageName при каждом вводе на input
-    handleNameChange = event => {
-        this.setState({imageName: event.currentTarget.value})
+  //обновляет imageName при каждом вводе на input
+  handleNameChange = event => {
+    this.setState({ imageName: event.currentTarget.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault(); //нет автоматической перезагрузке
+
+    //если запрос пуст - не отсылать; trim - учтет пробелы слева/справа
+    if (this.state.imageName.trim() === '') {
+      return alert('Please, enter what you want to find.');
     }
 
-    handleSubmit = (event) => {
-        event.preventDefault();   //нет автоматической перезагрузке
+    //вызываю метод из App и предаю ему значение imageName, и он вернется в  App
+    this.props.onSubmit(this.state.imageName);
+    this.setState({ imageName: '' });
+  };
 
-        //если запрос пуст - не отсылать; trim - учтет пробелы слева/справа
-        if (this.state.imageName.trim() === '') {
-            return alert ('Please, enter what you want to find.')
-        } 
+  render() {
+    return (
+      <header className="searchbar">
+        <form onSubmit={this.handleSubmit} className="form">
+          <button type="submit" className="button">
+            {/* <span className="button-label">Search</span> */}
+            <BiSearchAlt2 />
+          </button>
 
-        //вызываю метод из App и предаю ему значение imageName, и он вернется в  App
-        this.props.onSubmit(this.state.imageName);
-        this.setState({ imageName: '' });
-    
-} 
-
-    render() {
-        return (
-         <header className="searchbar">
-            <form onSubmit={this.handleSubmit} className="form">
-                <button type="submit" className="button">
-                        {/* <span className="button-label">Search</span> */}
-                        <BiSearchAlt2 />
-                       
-                </button>
-
-                <input
-                    className="input"
-                    type="text"
-                    value={this.state.imageName}
-                    onChange={this.handleNameChange}
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images and photos"
-                    />
-                    
-            </form>
-        </header>
-
-    )
+          <input
+            className="input"
+            type="text"
+            value={this.state.imageName}
+            onChange={this.handleNameChange}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
 }
-
-}
-
-
-  
-
